51edd0ae315cd72f074f2c04478e8eb1d17432b8
GangiReddy <gangireddy.ch@gmail.com>
2026-06-07 17:39:29 +0530

    fix(security): AI remediation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 import subprocess
+import shlex
+
+global host_list
+host_list = []
 
 app = FastAPI()
 
@@ -9,8 +13,7 @@ def home():
 
 @app.get("/ping")
 def ping(host: str):
-
-    # Vulnerable implementation
-    subprocess.call(f"ping {host}", shell=True)
-
+    if not any(host in h for h in host_list):
+        subprocess.call(shlex.split(f"ping {host}"))
+        host_list.append(host)
     return {"status": "completed"}
