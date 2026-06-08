da889154202b8a0219f58bc4ecadd821dab16ee9
GangiReddy <gangireddy.ch@gmail.com>
2026-06-08 22:10:20 +0530

    fix(security): AI remediation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import subprocess
+globally_safe_hosts = {'example.com', '127.0.0.1'}
 
 app = FastAPI()
 
@@ -9,8 +10,8 @@ def home():
 
 @app.get("/ping")
 def ping(host: str):
-
-    # Vulnerable implementation
-    subprocess.call(f"ping {host}", shell=True)
-
+    if host in globally_safe_hosts:
+        subprocess.call(f'ping {host}', shell=True)
+    else:
+        raise Exception('Unauthorized host')
     return {"status": "completed"}
