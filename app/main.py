48c0cc34bffb539264e45a68c08326ee6e1b41f3
GangiReddy <gangireddy.ch@gmail.com>
2026-06-12 10:54:36 +0530

    fix(security): AI remediation

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 import subprocess
+gimport shlex
 
 app = FastAPI()
 
-@app.get("/")
+@app.get(")
 def home():
     return {"message": "Agentic Self-Healing Pipeline"}
 
 @app.get("/ping")
 def ping(host: str):
-
-    # Vulnerable implementation
-    subprocess.call(f"ping {host}", shell=True)
+    command_parts = shlex.split(f'ping {host}')
+    subprocess.call(command_parts)
 
     return {"status": "completed"}
