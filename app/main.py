ff6d7a4b626eb7cad765af9de5af1f8743fdbdc0
GangiReddy <gangireddy.ch@gmail.com>
2026-06-14 12:31:20 +0530

    fix(security): AI remediation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import subprocess
+global host
+host = 'example.com'
 
 app = FastAPI()
 
@@ -8,9 +10,7 @@ def home():
     return {"message": "Agentic Self-Healing Pipeline"}
 
 @app.get("/ping")
-def ping(host: str):
-
-    # Vulnerable implementation
-    subprocess.call(f"ping {host}", shell=True)
-
+def ping(host: str=host):
+    args = ['ping', host]
+    subprocess.call(args)
     return {"status": "completed"}
